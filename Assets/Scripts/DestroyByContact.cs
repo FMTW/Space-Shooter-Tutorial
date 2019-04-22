7c3e78ca06d67074454655f517b1430c5796a513
FMTW <42906455+FMTW@users.noreply.github.com>
2019-04-22 21:21:50 +1000

    Update Tutorial

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -25,7 +25,10 @@ public class DestroyByContact : MonoBehaviour
 
         Instantiate(explosion, transform.position, transform.rotation);
         if (other.tag == "Player")
+        {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
         Destroy(other.gameObject);
         Destroy(gameObject);
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazzard;
+    public GameObject[] hazzards;
     [SerializeField] private GameObject player;
     [SerializeField] private int hazzardCounter;
     [SerializeField] private float startDelay;
@@ -12,12 +14,35 @@ public class GameController : MonoBehaviour
     [SerializeField] private float spawnDelay;
     [SerializeField] private Vector3 spawnValue;
     [SerializeField] private Quaternion spawnRotation;
-    [SerializeField] private TextMesh scoreText;
     [SerializeField] private int score;
+    [SerializeField] private bool restart;
+    [SerializeField] private bool gameover;
+
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI restartText;
+    [SerializeField] private TextMeshProUGUI gameoverText;
 
     private void Start()
     {
+        gameover = false;
+        restart = false;
+        restartText.text = "";
+        gameoverText.text = "";
+        score = 0;
+        UpdateScore();
         StartCoroutine(SpawnWave());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+           if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
 
     IEnumerator SpawnWave()
@@ -29,22 +54,35 @@ public class GameController : MonoBehaviour
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), 0, spawnValue.z);
                 spawnRotation = Quaternion.identity;
-                Instantiate(hazzard, spawnPosition, spawnRotation);
+                Instantiate(hazzards[Random.Range(0,hazzards.Length)], spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnDelay);
             }
-
+            
             yield return new WaitForSeconds(waveDelay);
+
+            if (gameover)
+            {
+                restartText.SetText("Press 'R' to Restart");
+                restart = true;
+                break;
+            }
         }
     }
 
     void UpdateScore()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.SetText("Score: {0}", score);
     }
 
     public void AddScore(int newScore)
     {
-        score = newScore;
+        score += newScore;
+        UpdateScore();
     }
 
+    public void GameOver()
+    {
+        gameoverText.SetText("Game Over");
+        gameover = true;
+    }
 }
